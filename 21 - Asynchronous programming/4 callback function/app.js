6aaf4b97c02ebd555ccf1b9f29ab62683875ed18
lyj-ooz <yujhin1026@gmail.com>
2021-02-21 23:22:55 +0900

    minor commit

---
+++ 21 - Asynchronous programming/4 callback function/app.js
@@ -0,0 +1,30 @@
+const posts = [
+  { id: 1, content: "this is the first post" },
+  { id: 2, content: "this is the second post" },
+];
+
+function createPost(post, cb) {
+  setTimeout(() => {
+    posts.push(post);
+    cb();
+  }, 2000);
+}
+
+function getPosts() {
+  setTimeout(() => {
+    let inner = "";
+    posts.forEach((post) => {
+      inner += `<li>${post.content}</li>`;
+    });
+    document.querySelector(".container").innerHTML = inner;
+  }, 1000);
+}
+
+// 아래처럼 하면 그냥 단순히 시간 순으로 실행되어 getPosts가 먼저 실행되어 버린다.
+// 콜백 함수를 활용해보자
+//createPost({ id: 2, content: "this is the 3rd post" });
+//getPosts(posts);
+
+// 콜백함수 활용하기
+// 1. createPost 함수 내에 콜백함수를 추가한다. 이 콜백함수는 getPosts를 호출한다.
+createPost({ id: 3, content: "this is the 3rd post" }, getPosts);
