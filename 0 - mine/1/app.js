20db281c86d04f046858a6a187916974748c3f79
lyj-ooz <yujhin1026@gmail.com>
2021-03-15 23:54:39 +0900

    엔터 누르면 인풋 입력값 받도록 수정, 인풋 클리어하는 함수 추가

--- 0 - mine/1/app.js
+++ 0 - mine/1/app.js
@@ -1,4 +1,4 @@
-function printWeatherInfo(data) {
+function showWeatherInfo(data) {
   document.querySelector(".show-weather").innerHTML = `
   <ul>
     <li>city name: ${data.name}</li>
@@ -8,15 +8,26 @@ function printWeatherInfo(data) {
     <li>temp min: ${data.main.temp_min}</li>
   </ul>
 `;
+  clearInput();
 }
 
-const url =
-  "http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=b3b2a40c3e5f4ba1ed64f02c07030015&units=metric";
-const w = new Weather();
+function clearInput() {
+  document.querySelector("#search-input").value = "";
+}
 
-w.getWeather(url) //
-  .then((res) => {
-    console.log(res);
-    printWeatherInfo(res);
-  })
-  .catch((err) => console.log(err));
+document.querySelector("#search-input").addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const searchInput = document.querySelector(".search #search-input").value;
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=b3b2a40c3e5f4ba1ed64f02c07030015&units=metric`;
+
+    const w = new Weather();
+
+    w.getWeather(url) //
+      .then((res) => {
+        console.log(res);
+        showWeatherInfo(res);
+      })
+      .catch((err) => console.log(err));
+  }
+});
