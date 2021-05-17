f736b6a11e755cfacdfe6d7fbf5e25c777934813
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-05-17 17:31:02 +0900

    light, dark 모드 관련 소스 추가

--- todo-dragNdrop/main.js
+++ todo-dragNdrop/main.js
@@ -5,14 +5,13 @@ const finishedCards = document.querySelector(".finished-cards");
 const todoCardsSection = document.querySelector(".todo");
 const finishedCardsSection = document.querySelector(".finished");
 const cards = document.querySelectorAll("div[class$='-cards']");
+const toggle = document.getElementById("toggle");
+
 let todos;
 let finished;
 
 init();
 
-// 삭제 버튼 누르면 해당 todo, finished 아이템을 삭제.
-// 처음 화면에는 삭제 버튼이 없으므로 직접 삭제 버튼에 이벤트리스너를 붙이지 못했다
-// 대신 -cards 클래스에 이벤트 리스너를 붙여서 이벤트 위임 방식으로 작성했다.
 cards.forEach((card) => {
   card.addEventListener("click", (e) => {
     if (e.target.className === "delete") {
@@ -31,7 +30,6 @@ cards.forEach((card) => {
   });
 });
 
-//todo-cards, finished-cards div에 마우스가 눌리면 div들을 싹 새로 불러온다음 drag 이벤트를 준다.
 container.addEventListener("mousedown", () => {
   const cardDivs = document.querySelectorAll(
     ".todo-cards div, .finished-cards div"
@@ -47,7 +45,6 @@ container.addEventListener("mousedown", () => {
   });
 });
 
-//finished div에 dragover 이벤트가 일어나면 finished-cards에 드래그로 끌고 온 div를 자식 요소로 추가한다.
 finishedCardsSection.addEventListener("dragover", (e) => e.preventDefault());
 finishedCardsSection.addEventListener("drop", (e) => {
   e.preventDefault();
@@ -63,7 +60,6 @@ finishedCardsSection.addEventListener("drop", (e) => {
   removeFromLocal(dragging.dataset.id, "todo");
 });
 
-//todo div에 dragover 이벤트가 일어나면 todo-cards에 드래그로 끌고 온 div를 자식 요소로 추가한다.
 todoCardsSection.addEventListener("dragover", (e) => e.preventDefault());
 todoCardsSection.addEventListener("drop", (e) => {
   e.preventDefault();
@@ -79,7 +75,6 @@ todoCardsSection.addEventListener("drop", (e) => {
   removeFromLocal(dragging.dataset.id, "finished");
 });
 
-// form 태그 안에 들어있는 input에 내용을 입력 후 엔터를 눌러도 form의 원래 액션이 일어나지 않도록 하기 위해서.
 document.querySelector(".input form").addEventListener("submit", (e) => {
   e.preventDefault();
 });
@@ -97,10 +92,8 @@ todoInput.addEventListener("keyup", (e) => {
     };
     addTodoToCards(inputVal, todoID);
     saveToLocal(todoData, "todo");
-
     todoInput.value = "";
   }
-
   e.preventDefault();
 });
 
@@ -109,6 +102,12 @@ todoInput.addEventListener("focusout", () => {
   todoInput.value = "";
 });
 
+//toggle for dark/light mode
+toggle.addEventListener("click", setDarkMode);
+
+//------------//
+// functions
+//------------//
 function init() {
   if (localStorage.getItem("todo")) {
     const todoFromLocal = JSON.parse(localStorage.getItem("todo"));
@@ -122,6 +121,14 @@ function init() {
     finishedFromLocal.forEach((finished) => {
       addTodoToCards(finished.val, finished.id, "finished");
     });
+  }
+
+  if (localStorage.getItem("dark")) {
+    const dark = JSON.parse(localStorage.getItem("dark"));
+    if (dark) {
+      toggle.classList.add("active");
+      document.body.classList.add("dark");
+    }
   }
 }
 
@@ -163,7 +170,7 @@ function saveToLocal(val, tag) {
     }
     todos.push(val);
     localStorage.setItem("todo", JSON.stringify(todos));
-  } else {
+  } else if (tag === "finished") {
     if (localStorage.getItem("finished")) {
       finished = JSON.parse(localStorage.getItem("finished"));
     } else {
@@ -171,12 +178,14 @@ function saveToLocal(val, tag) {
     }
     finished.push(val);
     localStorage.setItem("finished", JSON.stringify(finished));
+  } else {
+    localStorage.setItem("dark", JSON.stringify(val));
   }
 }
 
 function removeFromLocal(id, from) {
   if (from === "todo") {
-    // todo에서 지운다
+    // remove from 'todo'
     const todos = JSON.parse(localStorage.getItem("todo"));
     const idx = todos.findIndex((todo) => {
       return todo.id === id;
@@ -184,7 +193,7 @@ function removeFromLocal(id, from) {
     todos.splice(idx, 1);
     localStorage.setItem("todo", JSON.stringify(todos));
   } else {
-    // finished에서 지운다
+    // remove from 'finished'
     const finished = JSON.parse(localStorage.getItem("finished"));
     const idx = finished.findIndex((f) => {
       return f.id === id;
@@ -192,4 +201,22 @@ function removeFromLocal(id, from) {
     finished.splice(idx, 1);
     localStorage.setItem("finished", JSON.stringify(finished));
   }
+}
+
+function setDarkMode() {
+  let dark;
+  if (
+    toggle.classList.contains("active") &&
+    document.body.classList.contains("dark")
+  ) {
+    toggle.classList.remove("active");
+    document.body.classList.remove("dark");
+    dark = false;
+  } else {
+    toggle.classList.add("active");
+    document.body.classList.add("dark");
+    dark = true;
+  }
+
+  saveToLocal(dark, "dark");
 }
